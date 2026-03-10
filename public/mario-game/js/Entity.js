9bf64dd8d174b05455c6827645aa3ed651bdf968
tappakah1-dev <tappakah1@gmail.com>
2026-03-10 16:17:52 +0000

    sadad

--- public/mario-game/js/Entity.js
+++ public/mario-game/js/Entity.js
@@ -4,26 +4,16 @@ import BoundingBox from './BoundingBox.js';
 import EventBuffer from './EventBuffer.js';
 import Trait from './Trait.js';
 
+// --- CRITICAL FIX: Export Trait so other files can find it! ---
+export { Trait };
+
 export const Align = {
     center(target, subject) {
         subject.bounds.setCenter(target.bounds.getCenter());
     },
-
     bottom(target, subject) {
         subject.bounds.bottom = target.bounds.bottom;
-    },
-
-    top(target, subject) {
-        subject.bounds.top = target.bounds.top;
-    },
-
-    left(target, subject) {
-        subject.bounds.left = target.bounds.left;
-    },
-
-    right(target, subject) {
-        subject.bounds.right = target.bounds.right;
-    },
+    }
 };
 
 export const Sides = {
@@ -39,14 +29,12 @@ export default class Entity {
         this.audio = new AudioBoard();
         this.events = new EventBuffer();
         this.sounds = new Set();
-
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
         this.size = new Vec2(0, 0);
         this.offset = new Vec2(0, 0);
         this.bounds = new BoundingBox(this.pos, this.size, this.offset);
         this.lifetime = 0;
-
         this.traits = new Map();
     }
 
@@ -68,11 +56,9 @@ export default class Entity {
 
     finalize() {
         this.events.emit(Trait.EVENT_TASK, this);
-
         this.traits.forEach(trait => {
             trait.finalize(this);
         });
-
         this.events.clear();
     }
 
@@ -80,7 +66,6 @@ export default class Entity {
         this.sounds.forEach(name => {
             audioBoard.playAudio(name, audioContext);
         });
-
         this.sounds.clear();
     }
 
@@ -88,9 +73,7 @@ export default class Entity {
         this.traits.forEach(trait => {
             trait.update(this, gameContext, level);
         });
-
         this.playSounds(this.audio, gameContext.audioContext);
-
         this.lifetime += gameContext.deltaTime;
     }
 }
