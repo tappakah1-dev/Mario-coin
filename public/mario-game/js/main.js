9659ff5e52c42fbc39da43cd6fef4b2c78ecc60c
tappakah1-dev <tappakah1@gmail.com>
2026-03-10 16:11:52 +0000

    asdads

--- public/mario-game/js/main.js
+++ public/mario-game/js/main.js
@@ -82,7 +82,7 @@ async function main(canvas) {
             }
         });
 
-        level.comp.layers.push(createCollisionLayer(level));
+    //  level.comp.layers.push(createCollisionLayer(level));
 
         const dashboardLayer = createDashboardLayer(font, mario);
         level.comp.layers.push(dashboardLayer);
