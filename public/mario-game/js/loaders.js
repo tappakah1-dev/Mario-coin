b98064c17aca1615cce02174eb59897aa49d9ada
tappakah1-dev <tappakah1@gmail.com>
2026-03-10 14:41:44 +0000

    fgfdg

--- public/mario-game/js/loaders.js
+++ public/mario-game/js/loaders.js
@@ -1,14 +1,22 @@
-export function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
+import {loadImage} from '../loaders.js';
+import SpriteSheet from '../SpriteSheet.js';
 
-export function loadJSON(url) {
-    return fetch(url)
-    .then(r => r.json());
+export function loadFont() {
+    // REMOVED leading slash: '/img/font.png' -> 'img/font.png'
+    return loadImage('img/font.png')
+    .then(image => {
+        const fontSprite = new SpriteSheet(image, 8, 8);
+
+        const chars = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+        const size = 8;
+        const rowLen = image.width;
+        for (let [index, char] of [...chars].entries()) {
+            const x = index * size % rowLen;
+            const y = Math.floor(index * size / rowLen) * size;
+            fontSprite.define(char, x, y, size, size);
+        }
+
+        return fontSprite;
+    });
 }
